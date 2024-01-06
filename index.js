33cd20fa9c84862ac49cfe677ea83a62a8928a0a
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-06 10:29:10 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// code your solut
+function saturdayFun(activity = "roller-skate"){ 
+    return `This Saturday, I want to ${activity}!`; 
+}
+
+console.log(saturdayFun("bathe my dog !")) 
+// =>`This Saturday, I want to roller-skate'
+function mondayWork(activity = "go to the office"){
+
+    return `This Monday, I will ${activity}.`;
+
+}
+
+console.log(mondayWork("work from home"))
+
+// his Monday, I will go to the office
+function wrapAdjective(symbol) {
+    return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    }
+  }
